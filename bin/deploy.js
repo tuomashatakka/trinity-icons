87765f3481b1087052b2e4166d67069b1cb345e6
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2017-06-06 10:25:59 +0000

    Update deploy.js

--- bin/deploy.js
+++ bin/deploy.js
@@ -17,9 +17,9 @@ generateJSON({
   src: BASE_PATH,
   dst: 'public/icons.js' })
 
+generateArchive('assets/icons', 'assets/icons').then(()=>
 ncp(ASSETS_PATH, PUBLIC_ASSETS_PATH, err => {
   if (err) throw new Error('Assets directory could not be copied;', err)
   else {
-    generateArchive('assets/icons', 'assets/icons')
     generateIconFont()
-} })
+} }) ) 
