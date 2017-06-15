bf16e9107e07face4a7a2217b16299bd17b9382e
Tuomas Hatakka <tuomas.hatakka@fns.fi>
2017-06-16 00:46:38 +0300

    Fix deploy script to first generate zip files and then copy assets directory

--- bin/deploy.js
+++ bin/deploy.js
@@ -17,9 +17,8 @@ generateJSON({
   src: BASE_PATH,
   dst: 'public/icons.js' })
 
-ncp(ASSETS_PATH, PUBLIC_ASSETS_PATH, err => {
-  if (err) throw new Error('Assets directory could not be copied:', err)
-  else {
-    generateArchive('assets/icons', 'assets/icons')
-    generateIconFont()
-} })
+generateArchive('assets/icons', 'assets/icons')
+  .then(() => generateIconFont()
+  .then(() => ncp(ASSETS_PATH, PUBLIC_ASSETS_PATH, err => {
+    if (err) throw new Error('Assets directory could not be copied:', err)
+  })))
